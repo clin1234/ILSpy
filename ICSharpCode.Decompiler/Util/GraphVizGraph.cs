a7bf241513b6338fbd4dd222545669d8e9b15bf7
Charlie Lin <clin@rollins.edu>
2022-06-05 10:46:54 -0400

    Fix up source files (part 4)
    TODO:
    * Fix T4 files for assembly resolution
    * Replace BinaryFormatte

--- ICSharpCode.Decompiler/Util/GraphVizGraph.cs
+++ ICSharpCode.Decompiler/Util/GraphVizGraph.cs
@@ -18,6 +18,13 @@
 
 #nullable enable
 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+
 namespace ICSharpCode.Decompiler.Util
 {
 #if DEBUG
--- ICSharpCode.ILSpyCmd/IlspyCmdProgram.cs
+++ ICSharpCode.ILSpyCmd/IlspyCmdProgram.cs
@@ -92,9 +92,6 @@ Examples:
 		                                   typeof(FullTypeName).Assembly.GetName().Version;
 
 		[Option("-lv|--languageversion <version>", "C# Language version: CSharp1, CSharp2, CSharp3, " +
-			"CSharp4, CSharp5, CSharp6, CSharp7, CSharp7_1, CSharp7_2, CSharp7_3, CSharp8_0, CSharp9_0, " +
-			"CSharp10_0, Preview or Latest", CommandOptionType.SingleValue)]
-		public LanguageVersion LanguageVersion { get; } = LanguageVersion.Latest;
 		                                           "CSharp4, CSharp5, CSharp6, CSharp7_0, CSharp7_1, CSharp7_2, CSharp7_3, CSharp8_0, CSharp9_0, " +
 		                                           "CSharp_10_0 or Latest", CommandOptionType.SingleValue)]
 		private LanguageVersion LanguageVersion { get; } = LanguageVersion.Latest;
@@ -190,7 +187,8 @@ Examples:
 
 					return ListContent(fileName, output, kinds);
 				}
-				else if (ShowILCodeFlag || ShowILSequencePointsFlag)
+
+				if (ShowILCodeFlag || ShowILSequencePointsFlag)
 				{
 					if (outputDirectory != null)
 					{
@@ -200,7 +198,8 @@ Examples:
 
 					return ShowIL(fileName, output);
 				}
-				else if (CreateDebugInfoFlag)
+
+				if (CreateDebugInfoFlag)
 				{
 					string? pdbFileName = null;
 					if (outputDirectory != null)
@@ -215,7 +214,8 @@ Examples:
 
 					return GeneratePdbForAssembly(fileName, pdbFileName, app);
 				}
-				else if (DumpPackageFlag)
+
+				if (DumpPackageFlag)
 				{
 					return DumpPackageAssemblies(fileName, outputDirectory, app);
 				}
@@ -228,8 +228,14 @@ Examples:
 							(string.IsNullOrEmpty(TypeName) ? outputName : TypeName) + ".decompiled.cs"));
 					}
 
-					return Decompile(fileName, output, TypeName);
+				if (outputDirectorySpecified)
+				{
+					string outputName = Path.GetFileNameWithoutExtension(fileName);
+					output = File.CreateText(Path.Combine(OutputDirectory,
+						(string.IsNullOrEmpty(TypeName) ? outputName : TypeName) + ".decompiled.cs"));
 				}
+
+				return Decompile(fileName, output, TypeName);
 			}
 		}
 
